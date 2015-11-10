96bae31541f215448d4f55044b38cd29b7535c98
Eugenee Kutsenko <dessoya@gmail.com>
2015-11-11 03:13:24 +0600

    hash

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,20 @@
 
-var Class			= require('class')
-  , HashNavigator	= require('HashNavigator')
+var HashNavigator	= require('HashNavigator')
 
 module.exports = function() {
 
 	document.querySelector('body').innerHTML = require('layout.html')()
 
 	var navigator = new HashNavigator.Navigator
+
+	class Section1 extends HashNavigator.Section {
+		getRE() {
+			return 's1'
+		}
+	}
+
+	navigator.add(new Section1)
+
 	navigator.start()
 
 	console.log(navigator)
