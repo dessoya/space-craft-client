8da656e0cfef07f1d0c4b84a8d7ee6691c170473
Eugenee Kutsenko <dessoya@gmail.com>
2015-11-17 00:52:05 +0600

    css + hash event

--- modules/Menu/3.0/Item.ts
+++ modules/Menu/3.0/Item.ts
@@ -4,13 +4,25 @@
 export class Item extends UI_Control.Control {
 
 	private _item: IItem
+	private _state: boolean = false
+
 	constructor(item: IItem) {
 		super()
 		this._item = item
 	}
 
 	onHashChange(hash: string) {
-		console.log('Menu.Item.onHashChange ' + this._item.title + ' ' + hash)
+		var state: boolean = this._item.hash === hash
+		if (this._state !== state) {
+			this._state = state
+
+			if (state) {
+				this.el.classList.add('active')
+			}
+			else {
+				this.el.classList.remove('active')
+			}
+		}
 	}
 
 	render(): string {
